6e06b6dc2da4b9632ea0b92f3d3aa5359c93b856
keerthi <kgoli@nisum.com>
2020-01-24 18:34:29 +0530

    my first commit

--- src/main/java/com/nisum/SpringBootPOC/SpringBootPocApplication.java
+++ src/main/java/com/nisum/SpringBootPOC/SpringBootPocApplication.java
@@ -5,5 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringBootPocApplication {
 public static void main(String[] args) {
 		SpringApplication.run(SpringBootPocApplication.class, args);
+		
+		System.out.println("first commit");
 	}
 }
